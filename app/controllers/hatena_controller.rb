07b342c74b6f57d23b13d485d7684ab17784642f
hotuta <5bjm1101@gmail.com>
2015-11-28 10:20:57 +0000

    5 commit

--- app/controllers/hatena_controller.rb
+++ app/controllers/hatena_controller.rb
@@ -3,6 +3,7 @@
 class HatenaController < ApplicationController
 
   API_URL = "http://b.hatena.ne.jp/entry/json/"
+  #STAR = "http://s.st-hatena.com/entry.count.image?uri="
 
   def search
     require 'open-uri'
@@ -14,21 +15,25 @@ class HatenaController < ApplicationController
     @comments = Array.new(parsed.length)
     @users = Array.new(parsed.length)
     @tags = Array.new(parsed.length)
+    
+    
 
     if @url.blank?
       @url = ""
       @alert = "URLを入力してください。"
       return
     end
+    
+    
 
     # 入力されたURLをJSONで展開する
     begin
+      @url =URI.decode(@url)
       html = open(API_URL+@url).read
       json = JSON.parser.new(html)
       hash=json.parse()
-
     rescue
-      @alert="URLが間違っています。\n正しく入力してください。"
+      @alert="まだブックマークされていないかURLが間違っています。\n正しく入力してください。"
       return
     end
 
@@ -36,16 +41,23 @@ class HatenaController < ApplicationController
       parsed = hash['bookmarks']
       @title = hash['title']
       @entry_url = hash['entry_url']
+      
+
     else
       @alert="データがありません。"
       return
     end
     
+    begin
     parsed.each_with_index do |item, i|
       @timestamps[i] = item['timestamp']
       @comments[i] = item['comment']
       @users[i] = item['user']
       @tags[i] = item['tags']
+    end  
+    rescue
+      @alert="コメント一覧を非表示に設定されているみたいです。"
+    return
     end
   end
 
